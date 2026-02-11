7ad4119e62f49153efa0b209a8eba0178b34df9f
Thilo <thilo0123@gmail.com>
2026-02-11 06:31:00 +0100

    Add admin to "logo" if we're on admin routes

--- app/components/AppLogo.vue
+++ app/components/AppLogo.vue
@@ -4,6 +4,6 @@ const { appName } = useBackEndConfig()
 
 <template>
   <h1 class="flex items-center gap-2 text-3xl font-extralight text-primary-600">
-    {{ appName }}
+    {{ appName }} <span v-if="$route.path.includes('admin')" class="italic text-red-600 dark:text-red-400">Admin</span>
   </h1>
 </template>
