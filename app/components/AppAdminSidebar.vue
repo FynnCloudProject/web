d154851f8e293cf6b467977d33f07434beb7fc01
Thilo <thilo0123@gmail.com>
2026-02-19 00:27:01 +0100

    Add tiers to admin panel

--- app/components/AppAdminSidebar.vue
+++ app/components/AppAdminSidebar.vue
@@ -8,13 +8,14 @@ const links = computed(() => [
     {
         label: t('admin.users'), icon: 'heroicons:users-16-solid', to: '/admin/users'
     },
+    { label: t('admin.tiers'), icon: 'heroicons:circle-stack-solid', to: '/admin/tiers' },
     { label: t('admin.settings'), icon: 'heroicons:cog-6-tooth-solid', to: '/admin/settings' },
 ])
 </script>
 
 <template>
     <div class="h-full">
-        <div class="h-full bg-primary-950 dark:bg-primary-950 shadow-lg pb-2 flex flex-col justify-between">
+        <div class="h-full bg-primary-700 dark:bg-primary-800 shadow-lg pb-2 flex flex-col justify-between">
             <nav class="space-y-1 p-2">
                 <AppSidebarItem v-for="link in links" :key="link.to" :link="link" />
             </nav>
