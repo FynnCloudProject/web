d8ea3a7a096b6f528c8bd4280d99373ac6531e4d
Thilo <thilo0123@gmail.com>
2026-05-28 07:52:58 +0200

    Fix toast anim

--- app/components/AppToastContainer.vue
+++ app/components/AppToastContainer.vue
@@ -5,12 +5,12 @@ const { toasts, removeToast } = useToast()
 <template>
     <Teleport to="body">
         <div
-            class="fixed bottom-0 right-0 z-200 flex flex-col gap-3 pointer-events-auto max-h-[calc(100vh)] overflow-y-auto p-4">
+            class="fixed bottom-0 right-0 z-200 flex flex-col gap-3 pointer-events-auto max-h-[calc(100vh)] overflow-visible p-4">
             <TransitionGroup
-                enter-active-class="transition-[transform,opacity] duration-400 ease-out will-change-[transform,opacity]"
-                enter-from-class="opacity-0 translate-x-24" enter-to-class="opacity-100 translate-x-0"
-                leave-active-class="transition-[transform,opacity] duration-250 ease-in will-change-[transform,opacity]"
-                leave-from-class="opacity-100 translate-x-0" leave-to-class="opacity-0 translate-x-24"
+                enter-active-class="transition-[transform,opacity] duration-400 ease-[cubic-bezier(0.16,1,0.3,1)] will-change-[transform,opacity]"
+                enter-from-class="opacity-0 translate-x-full" enter-to-class="opacity-100 translate-x-0"
+                leave-active-class="transition-[transform,opacity] duration-300 ease-in will-change-[transform,opacity]"
+                leave-from-class="opacity-100 translate-x-0" leave-to-class="opacity-0 translate-x-full"
                 move-class="transition-transform duration-300 ease-out">
                 <div v-for="toast in toasts" :key="toast.id">
                     <AppToast :toast="toast" :on-close="removeToast" :duration="5000" />
